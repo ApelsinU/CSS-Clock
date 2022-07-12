0af07127c9c155526be13f43aecb3027ba25afc4
Vadim Kaptsov <vadimkaptsov@mail.ru>
2022-07-12 11:41:07 +0300

    add digit clock, refactor js

--- js/app.js
+++ js/app.js
@@ -4,20 +4,39 @@ document.addEventListener("DOMContentLoaded", () => {
   const min = document.getElementById("min")
   const sec = document.getElementById("sec")
 
+  const dgHour = document.getElementById("dg-hour")
+  const dgMin = document.getElementById("dg-min")
+  const dgSec = document.getElementById("dg-sec")
 
-  setInterval(()=> {
+  const timePref = document.getElementById("time-prefix")
+
+  setInterval(() => {
     const day = new Date();
+    const h = day.getHours();
+    const m = day.getMinutes();
+    const s = day.getSeconds();
 
-    const hourAngle = day.getHours() * 30;
-    const minAngle = day.getMinutes() * 6;
-    const secAngle = day.getSeconds() * 6;
+    setClockTime(h, m, s)
+    setDigitTime(h, m, s)
+  })
+
+
+  function setClockTime(h, m, s) {
+    const hourAngle = h * 30;
+    const minAngle = m * 6;
+    const secAngle = s * 6;
 
     hour.style.transform = `rotateZ(${hourAngle + (minAngle/12)}deg)`
     min.style.transform = `rotateZ(${minAngle}deg)`
     sec.style.transform = `rotateZ(${secAngle}deg)`
+  }
 
-    console.log(secAngle)
-  })
+  function setDigitTime(h, m, s) {
+    h = h > 12 ? h - 12 : h;
 
-
+    dgHour.innerHTML = h < 10 ? `0${h}` : h;
+    dgMin.innerHTML = m < 10 ? `0${m}` : m;
+    dgSec.innerHTML = s < 10 ? `0${s}` : s;
+    timePref.innerHTML = h > 12 ? 'PM' : 'AM';
+  }
 });
